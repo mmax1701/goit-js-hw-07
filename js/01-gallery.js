75a70a49b62c5de695f911328a77ba2ccb2a6a2f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-05-20 19:21:33 +0300

    hw 7.1

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+function createGalleryItem({ original, preview, description }) {
+    const galleryItemEl = document.createElement('li');
+    galleryItemEl.classList.add('gallery__item');
+
+    const galleryLinkEl = document.createElement('a');
+    galleryLinkEl.classList.add('gallery__link');
+    galleryLinkEl.href = original;
+
+    const galleryImageEl = document.createElement('img');
+    galleryImageEl.classList.add('gallery__image');
+    galleryImageEl.src = preview;
+    galleryImageEl.dataset.source = original;
+    galleryImageEl.alt = description;
+
+    galleryLinkEl.appendChild(galleryImageEl);
+    galleryItemEl.appendChild(galleryLinkEl);
+
+    return galleryItemEl;
+}
+
+const galleryMarkup = galleryItems.map(createGalleryItem);
+galleryEl.append(...galleryMarkup);
+
+galleryEl.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const largeImageURL = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    galleryEl.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+})
+})
+
+
+console.log(galleryItems);
